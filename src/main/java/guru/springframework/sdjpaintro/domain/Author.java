2846b51ee8d6b3cb8d7bd6fa0bf52f83da8cfa8b
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2023-01-31 16:52:57 +0200

    added table via liquibase

---
+++ src/main/java/guru/springframework/sdjpaintro/domain/Author.java
@@ -0,0 +1,24 @@
+package guru.springframework.sdjpaintro.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+}
