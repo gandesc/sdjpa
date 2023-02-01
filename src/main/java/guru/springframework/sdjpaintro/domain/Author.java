7fbd248922094f0d894d3db3102162b9ecbf4c5e
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2023-02-01 17:47:28 +0200

    added auto incr with liq

--- src/main/java/guru/springframework/sdjpaintro/domain/Author.java
+++ src/main/java/guru/springframework/sdjpaintro/domain/Author.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 public class Author {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String firstName;
--- src/main/java/guru/springframework/sdjpaintro/domain/Book.java
+++ src/main/java/guru/springframework/sdjpaintro/domain/Book.java
@@ -16,7 +16,7 @@ import lombok.*;
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long authorId;
